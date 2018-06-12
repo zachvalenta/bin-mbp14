f654e4ecc9ac116b81fbda65df933f43f521552b
z_jay_v <zjv5002@gmail.com>
2018-06-12 14:25:48 -0400

    Update fne.py

--- fne.py
+++ fne.py
@@ -7,30 +7,42 @@ def print_names(names, msg):
 		print(name)
 	print("\n")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--start", help="starting index for file name slice", type=int, nargs='?', default=None)
-parser.add_argument("-e", "--end", help="ending index for file name slice", type=int, nargs='?', default=None)
-args = parser.parse_args()
 
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--start", help="starting index for file name slice", type=int, nargs='?', default=None)
+	parser.add_argument("-e", "--end", help="ending index for file name slice", type=int, nargs='?', default=None)
+	parser.add_argument("-l", "--lowercase", help="lowercase file names")
+	return parser.parse_args()
+
+
+def execute_edits():
+	user_confirmation = input("look good? (RETURN to execute ||| 'r' to redo args ||| 'q' to quit) ➡️   ")
+	if(user_confirmation==""):
+		print('modification confirmed')
+		for orig, new in zip(orig_names, new_names):
+			os.rename(orig, new)
+		print('🔼  modification complete')
+	else:
+		print('🔽  modification aborted')
+
+# take inputs
+args = parse_args()
+
+# store original filenames
 orig_names = [x for x in os.listdir('.') if x[-3:]=="mp3" and os.path.isfile(x)]
 print_names(orig_names, "original names")
 
+# store proposed new filenames
 if(args.end):
 	args.end = -args.end
 	new_names = [x[args.start:args.end-4]+".mp3" for x in os.listdir('.') if x[-3:]=="mp3" and os.path.isfile(x)]
 else:
 	new_names = [x[args.start:]+".mp3" for x in os.listdir('.') if x[-3:]=="mp3" and os.path.isfile(x)]
-
 print_names(new_names, "updated names")
 
-user_confirmation = input("look good? (RETURN to execute ||| 'r' to redo args ||| 'q' to quit) ➡️   ")
-if(user_confirmation==""):
-	print('modification confirmed')
-	for orig, new in zip(orig_names, new_names):
-		os.rename(orig, new)
-	print('🔼  modification complete')
-else:
-	print('🔽  modification aborted')
+execute_edits()
+
 
 """
 TODOs:
